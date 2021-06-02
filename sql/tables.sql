8ce5b0be0e993739f88876ba5a22729a86277627
schianodicola <85114291+schianodicola@users.noreply.github.com>
2021-06-02 23:32:21 +0200

    Log: Create DB; Implemented query in ContactDaoImpl;

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,6 +1,45 @@
-CREATE TABLE `contacts` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(25) DEFAULT NULL,
-  `surname` varchar(25) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE contacts (
+  id bigint unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(25) DEFAULT NULL,
+  surname varchar(25) DEFAULT NULL,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE mail (
+  id_contacts bigint unsigned,
+  email varchar(25) DEFAULT NULL
+  
+);
+ALTER TABLE mail
+ADD CONSTRAINT mail_contact_fk1 FOREIGN KEY (id_contacts) REFERENCES contacts(id);
+
+
+CREATE TABLE phone (
+  id_contacts bigint unsigned,
+  number varchar(25) DEFAULT NULL,
+  
+  CONSTRAINT phone_contact_fk1 FOREIGN KEY (id_contacts) REFERENCES contacts(id)
+  
+);
+
+
+
+INSERT INTO contacts (name,surname) VALUES
+('Giuseppe', 'Schiano'),
+('Franci', 'Petri'),
+('Annalisa', 'Rossi'),
+('Andrea', 'Verdi');
+
+INSERT INTO mail (id_contacts,email) VALUES
+(1, 'aaa@mail.it'),
+(2, 'bbb@mail.it'),
+(3, 'ccc@mail.it'),
+(4, 'ddd@mail.it'),
+(4, 'provola@mail.it');
+
+INSERT INTO phone (id_contacts,number) VALUES
+(1, '33311111'),
+(2, '33322222'),
+(3, '33333333'),
+(4, '33344444');
+
--- src/it/rdev/rubrica/model/impl/rdbms/ContactDAOImpl.java
+++ src/it/rdev/rubrica/model/impl/rdbms/ContactDAOImpl.java
@@ -3,6 +3,7 @@ package it.rdev.rubrica.model.impl.rdbms;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import it.rdev.rubrica.model.Contact;
@@ -11,18 +12,44 @@ import it.rdev.rubrica.model.ContactDAO;
 public class ContactDAOImpl extends AbstractDAO<Contact> implements ContactDAO {
 	
 	private final String TABLE_NAME = "contacts";
+	private final String TABLE_MAIL = "mail";
+	private final String TABLE_PHONE = "phone";
 
 	public List<Contact> getAll() {
 		List<Contact> contacts = new ArrayList<>();
 		try {
 			ResultSet rs = this.executeQuery("SELECT id, name, surname FROM " + TABLE_NAME);
+			Contact c= new Contact();
 			while(rs.next()) {
-				contacts.add(
-						new Contact()
-						.setId(rs.getInt("id"))
-						.setName(rs.getString("name"))
-						.setSurname(rs.getString("surname")));
+				//contacts.add(
+						//new Contact()
+						c.setId(rs.getInt("id"));
+						c.setName(rs.getString("name"));
+						c.setSurname(rs.getString("surname"));
+			//}
+			//System.out.println("IDDD "+ c.getId());
+			
+			rs = this.executeQuery("SELECT email FROM " + TABLE_MAIL +" WHERE id_contacts=" +c.getId());
+			List<String> emails=new ArrayList();
+			while(rs.next()) {
+						//new Contact()
+						emails.add(rs.getString("email"));
 			}
+			c.setEmails(emails);
+			
+			rs = this.executeQuery("SELECT number FROM " + TABLE_PHONE +" WHERE id_contacts='" +c.getId());
+			List<String> phoneNumbers=new ArrayList();
+			while(rs.next()) {
+						//new Contact()
+						phoneNumbers.add(rs.getString("number"));
+			}
+			c.setPhoneNumbers(phoneNumbers);
+			
+			}//fine primo while
+			contacts.add(c);
+
+			
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -31,17 +58,77 @@ public class ContactDAOImpl extends AbstractDAO<Contact> implements ContactDAO {
 
 	@Override
 	public boolean persist(Contact o) throws SQLException {
+		
+		long idc= this.executeInsert("Insert into contacts (name,surname) VALUES(?,?)", o.getName(), o.getSurname() );
+		long ide=0;
+		long idn=0;
+		for(int i=0; i< o.getEmails().size();i++) {
+			ide=this.executeInsert("Insert into" +TABLE_MAIL+" (id_contacts, email) VALUES(?,?)", o.getId(), o.getEmails().get(i));
+			
+		}
+		
+		for(int i=0; i< o.getPhoneNumbers().size();i++) {
+			idn=this.executeInsert("Insert into" +TABLE_PHONE+" (id_contacts, number) VALUES?(?)", o.getId(), o.getPhoneNumbers().get(i));
+			
+		}
+		if(idc!=0 && ide!=0 && idn!=0) return true;
 		return false;
+
 	}
 
 	@Override
 	public boolean delete(Contact t) throws SQLException {
-		return false;
+
+		try {
+			this.executeUpdate("DELETE FROM contacts WHERE id=?", t.getId());
+		}catch(SQLException e) {
+			System.err.println("ERRORE - Delete");
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean update(Contact t) throws SQLException {
-		return false;
+		this.executeUpdate("DELETE FROM emails WHERE id_contact = ?", t.getId());
+		this.executeUpdate("DELETE FROM phones WHERE id_contact = ?", t.getId());
+		
+		insertEmailAndPhone(t, t.getId());
+		return true;
+	}
+	
+	private void insertEmailAndPhone(Contact c, Integer id) throws SQLException {
+		// numero di insert ottimizzato
+		
+		if( c.getEmails() != null ) {
+			Object[] emailsParam = new Object[c.getEmails().size() * 2];
+			Iterator<String> emailIt = c.getEmails().iterator();
+			StringBuffer sb = new StringBuffer("INSERT INTO emails VALUES ");
+			for(int i = 0; i<emailsParam.length && emailIt.hasNext();) {
+				if( i > 0) {
+					sb.append(", ");
+				}
+				sb.append("(?, ?)");
+				emailsParam[i++] = emailIt.next();
+				emailsParam[i++] = id;
+			}
+			this.executeUpdate(sb.toString(), emailsParam);
+		}
+		
+		if( c.getPhoneNumbers() != null ) {
+			Object[] phonesParam = new Object[c.getPhoneNumbers().size() * 2];
+			Iterator<String> phoneIt = c.getPhoneNumbers().iterator();
+			StringBuffer sb = new StringBuffer("INSERT INTO phones VALUES ");
+			for(int i = 0; i<phonesParam.length && phoneIt.hasNext();) {
+				if( i > 0) {
+					sb.append(", ");
+				}
+				sb.append("(?, ?)");
+				phonesParam[i++] = phoneIt.next();
+				phonesParam[i++] = id;
+			}
+			this.executeUpdate(sb.toString(), phonesParam);
+		}
 	}
 
 }
